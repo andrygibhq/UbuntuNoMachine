ffad7208f62efcde12dd0164e75cea78ab8c58a8
andrygibhq <andrygibhq@gmail.com>
2024-02-20 14:35:55 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,10 +4,9 @@ stty quit ""
 stty susp undef
 
 clear
-rm -rf ngrok ngrok.tgz ng.sh > /dev/null 2>&1
+rm -rf ngrok ngrok.tar.gz ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar xvzf ngrok.tgz
-
+wget -O ngrok.tar.gz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xvzf ngrok.tar.gz > /dev/null 2>&1
