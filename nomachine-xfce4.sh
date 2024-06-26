3d8ebed9feca927e70b03109a30035c9f11527e7
andrygibhq <andrygibhq@gmail.com>
2024-06-26 11:42:20 +0700

    Update nomachine-xfce4.sh

--- nomachine-xfce4.sh
+++ nomachine-xfce4.sh
@@ -34,13 +34,13 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:8080/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:xfce4
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
 echo IP Address:
-curl --silent --show-error http://127.0.0.1:8080/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
